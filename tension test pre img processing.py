6885170be61999eae795bd4fa72a8e0d70817e3a
HOGEUN SHIN <57739012+HOGEUNGOD@users.noreply.github.com>
2020-03-25 00:53:32 +0900

    add tension test

--- tension test pre img processing.py
+++ tension test pre img processing.py
@@ -3,15 +3,20 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 import PIL.Image as pil
+import os
 
 '''at tension test, useint the for clause and reading reverse order 
   then calculate array average difference'''
 
-path_plus = 'data/'
+path = 'data/img'
 
-path = 'Stress-ML2.xlsx'
-data = pd.read_excel(path_plus+path)
 
+file_list = os.listdir(path)
+for file in file_list:
+    if file.endswith(".jpg"):
+        im = pil.open(os.path.join(path, file))
+
+'''
 #이미지 배열 처리하는 부분
 im = pil.open(path_plus+'ff000001.png')
 im2 = pil.open('.png')
@@ -19,3 +24,4 @@ im2 = pil.open('.png')
 im_array_original = np.array(im)
 im_array_last = np.array(im2)
 img_difference = np.array(im2)-np.array(im)
+'''
