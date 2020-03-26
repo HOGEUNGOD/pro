53ed38cae6c2807fb0c0e1f60bae04cb28d87e28
HOGEUN SHIN <57739012+HOGEUNGOD@users.noreply.github.com>
2020-03-26 18:59:46 +0900

    이미지 프로세서

--- tension test pre img processing.py
+++ tension test pre img processing.py
@@ -1,27 +1,51 @@
 import numpy as np
-import pandas as pd
-import csv
-import matplotlib.pyplot as plt
-import PIL.Image as pil
+import cv2
 import os
 
-'''at tension test, useint the for clause and reading reverse order 
-  then calculate array average difference'''
 
-path = 'data/img'
+"""setting values"""
+path = 'data/igm/'
+#part1
+fisrt_img = 'ff000704.jpg'
+last_img = 'ff002448.jpg'
+#part2 , location_szie must be even
+center_location = (150, 150)
+location_size_width = 20
+location_size_length = 20
 
 
-file_list = os.listdir(path)
-for file in file_list:
-    if file.endswith(".jpg"):
-        im = pil.open(os.path.join(path, file))
 
-'''
-#이미지 배열 처리하는 부분
-im = pil.open(path_plus+'ff000001.png')
-im2 = pil.open('.png')
-
+################part1##############################
+im = cv2.imread(path+fisrt_img, cv2.IMREAD_GRAYSCALE)
+im2 = cv2.imread(path+last_img, cv2.IMREAD_GRAYSCALE)
 im_array_original = np.array(im)
 im_array_last = np.array(im2)
 img_difference = np.array(im2)-np.array(im)
-'''
+
+cv2.imwrite("models/i.jpg", img_difference)
+
+
+#################part2#############################
+file_list = os.listdir(path)
+count = 0
+length = location_size_length/2
+width = location_size_width/2
+result = []
+
+slice_x1 = int(center_location[1]-width)
+slice_x2 = int(center_location[1]+width)
+slice_y1 = int(center_location[0]-length)
+slice_y2 = int(center_location[0]+length)
+
+
+for file in file_list:
+    if file.endswith(".jpg"):
+        im = cv2.imread(os.path.join(path, file), cv2.IMREAD_GRAYSCALE)
+        im_calculate = np.array(im)
+        img_box = im_calculate[slice_y1:slice_y2, slice_x1:slice_x2]
+        ave = np.average(img_box)
+        result = np.append(result, [ave])
+        count += 1
+
+
+        print(np.shape(im_calculate), count)
