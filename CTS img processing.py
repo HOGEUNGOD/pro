ab7bf7840f4965a0df6cc9e7a0ee96a2a303394d
HOGEUN SHIN <57739012+HOGEUNGOD@users.noreply.github.com>
2020-03-28 02:06:37 +0900

    이미지 프로세서

---
+++ CTS img processing.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+import os
+import matplotlib as plt
+
+path = 'data/img/'
+im_first = 'ff000003.jpg'
+save_path = 'models/'
+
+file_list = os.listdir(path)
+count = 0
+for file in file_list:
+    if file.endswith(".jpg"):
+        im1 = cv2.imread(path+ im_first, cv2.IMREAD_GRAYSCALE)
+        im2 = cv2.imread(os.path.join(path, file), cv2.IMREAD_GRAYSCALE)
+        im_calculate_first = np.array(im1)
+        im_calculate = np.array(im2)
+        img_difference = im_calculate - im_calculate_first
+        cv2.imwrite(save_path+file, img_difference)
+
+        count += 1
+
+
+plt.xlabel('degree')
+plt.ylabel('f1')
+plt.legend()
+plt.xlim(0,90)
+plt.grid()
--- tension test pre img processing.py
+++ tension test pre img processing.py
@@ -1,17 +1,18 @@
 import numpy as np
 import cv2
 import os
-
+import matplotlib.pylab as plt
+import pandas as pd
 
 """setting values"""
-path = 'data/igm/'
+path = 'data/composite/'
 #part1
-fisrt_img = 'ff000704.jpg'
-last_img = 'ff002448.jpg'
+fisrt_img = 'ff000001.jpg'
+last_img = 'ff000423.jpg'
 #part2 , location_szie must be even
-center_location = (150, 150)
-location_size_width = 20
-location_size_length = 20
+center_location = (424, 562)
+location_size_width = 200
+location_size_length = 200
 
 
 
@@ -49,3 +50,23 @@ for file in file_list:
 
 
         print(np.shape(im_calculate), count)
+
+y1 = result
+
+xl = pd.read_csv('data/wvd_000_WaveData.csv')
+data = np.array(xl)
+x = data[:, 0]
+y2 = data[:, 3]
+
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+
+line1 = ax1.plot(x ,y1, color='b', label="ML")
+line2 = ax2.plot(x, y2, color='r', label="STRESS NONE CONVERT")
+
+lines = line1 + line2
+labels = [l.get_label() for l in lines]
+plt.legend(lines, labels, loc=0)
+
+
+plt.savefig('models\graph.png', dpi=600)
